ef9e5303af4690b60b59c2a6f6e799ba6b18af4a
Aditya Giri <adityagiri1911@gmail.com>
2025-06-18 11:50:27 +0530

    again changed made

--- Prog.java
+++ Prog.java
@@ -1,6 +1,6 @@
 public class Prog {
     public static void main(String[] args) {
         System.out.println("Jai Shree Radhe!");
-        System.out.println("Jai Shree Ram!");
+        System.out.println("Jai Shree Ram!!");
     }
 }
